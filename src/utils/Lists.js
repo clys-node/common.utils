fd4fe54537b415bb1484f3f1b8c8d9ffbb4bd8c9
nightNya <540961620@qq.com>
2020-05-18 16:43:33 +0800

    includesAll

--- src/utils/Lists.js
+++ src/utils/Lists.js
@@ -1,11 +1,30 @@
 const Objs = require('./Objs');
 
 const Lists = {
-  isEmpty(l) {
-    return Objs.isNull(l) || l.length < 1;
+  /**
+   *
+   * @param {Array} list
+   * @returns {boolean}
+   */
+  isEmpty(list) {
+    return Objs.isNull(list) || list.length < 1;
   },
-  isNotEmpty(l) {
-    return !this.isEmpty(l);
+  /**
+   *
+   * @param {Array} list
+   * @returns {boolean}
+   */
+  isNotEmpty(list) {
+    return !this.isEmpty(list);
+  },
+  /**
+   *
+   * @param {Array} whole
+   * @param {Array} part
+   * @returns {boolean}
+   */
+  includesAll(whole = [], part = []) {
+    return part.every((v) => whole.includes(v));
   }
 };
 
